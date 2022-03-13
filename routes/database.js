af3e27d49738f850c3c2b00130f44596d8174d33
hass0319 <amro.abdrabou@hotmail.co.uk>
2022-03-13 18:55:23 +0000

    after james pull

--- routes/database.js
+++ routes/database.js
@@ -6,35 +6,33 @@ const pool = new Pool({
   user: 'labber',
   password: 'labber',
   host: 'localhost',
-  database: 'Midterm'
+  database: 'midterm'
 });
 
-
-
-const addUser =  function(user) {
-  const values = [user.name, user.email, user.password]
+const addUser = function (user) {
+  const values = [user.name, user.email, user.password];
   return pool.query(`INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *;`, values)
-  .then((result) => {
-    console.log(result.rows);
-    return (result.rows);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-}
+    .then((result) => {
+      console.log(result.rows);
+      return (result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.addUser = addUser;
 
-const getUserWithEmail = function(email) {
+const getUserWithEmail = function (email) {
   return pool
-  .query(`SELECT * FROM users WHERE email = $1;`, [email])
-  .then((result) => {
-    console.log(result.rows[0]);
-    return result.rows[0];
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-}
+    .query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 // getUserWithEmail()
 
 exports.getUserWithEmail = getUserWithEmail;
