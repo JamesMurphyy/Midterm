9bcf83cfb2001de144ca8add967e6188252716c0
James Murphy <jamesm8228@gmail.com>
2022-03-15 17:16:50 -0400

    Add delete button

---
+++ routes/edit.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM posts WHERE vendor_id = $1`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+
+
+  return router;
+};
